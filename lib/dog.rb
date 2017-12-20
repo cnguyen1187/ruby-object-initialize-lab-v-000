a37ee2778597f140abcd0e0c0c6f3fb1d9ac7cd6
Cam Nguyen <cnguyen1187@gmail.com>
2017-12-20 23:09:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,17 @@
 class Dog
+    def initialize(name)
+      @name = (name)
+    end
+
+    def name=(name)
+      @name = (name)
+    end
+
+    def name
+      @name
+    end
+  end
+
   def initialize(breed)
     @breed = (breed)
   end
